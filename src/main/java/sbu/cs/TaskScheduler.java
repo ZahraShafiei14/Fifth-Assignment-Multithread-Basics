da4756d990d8adfa53297b88672786c4690e6ddd
Zahra <zahrashafiei14@gmail.com>
2024-05-12 05:26:48 +0330

    TaskScheduler class completed.

--- src/main/java/sbu/cs/TaskScheduler.java
+++ src/main/java/sbu/cs/TaskScheduler.java
@@ -1,15 +1,13 @@
 package sbu.cs;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Comparator;
 
 public class TaskScheduler
 {
+
     public static class Task implements Runnable
     {
-        /*
-            ------------------------- You don't need to modify this part of the code -------------------------
-         */
         String taskName;
         int processingTime;
 
@@ -17,35 +15,29 @@ public class TaskScheduler
             this.taskName = taskName;
             this.processingTime = processingTime;
         }
-        /*
-            ------------------------- You don't need to modify this part of the code -------------------------
-         */
-
         @Override
         public void run() {
-            /*
-            TODO
-                Simulate utilizing CPU by sleeping the thread for the specified processingTime
-             */
+            try { Thread.sleep(processingTime); }
+            catch (InterruptedException e){ e.printStackTrace(); }
         }
     }
 
     public static ArrayList<String> doTasks(ArrayList<Task> tasks)
     {
         ArrayList<String> finishedTasks = new ArrayList<>();
+        tasks.sort(Comparator.comparingInt(task -> -task.processingTime));
 
-        /*
-        TODO
-            Create a thread for each given task, And then start them based on which task has the highest priority
-            (highest priority belongs to the tasks that take more time to be completed).
-            You have to wait for each task to get done and then start the next task.
-            Don't forget to add each task's name to the finishedTasks after it's completely finished.
-         */
+        for (Task task: tasks){
+            Thread t = new Thread(task);
+            t.start();
+            try { t.join(); }
+            catch (InterruptedException e){ e.printStackTrace(); }
 
+            finishedTasks.add(task.taskName);
+        }
         return finishedTasks;
     }
 
     public static void main(String[] args) {
-        // Test your code here
     }
 }
